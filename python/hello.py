09ac46d66a798f4dff32b2dda06519af011a056d
earlymor <1162907534@qq.com>
2023-01-01 15:55:11 +0800

    helloworld

---
+++ python/hello.py
@@ -0,0 +1,31 @@
+# print("hello world")#注释1
+# '''
+# 注释2
+# '''
+# """
+# 注释3
+# """
+# print("你好")
+# a=True
+# print(a)
+# print(type(a))#布尔类型bool
+# b=()
+# print(b)
+# print(type(b))#元组类型tuple
+# c=[]
+# print(c)
+# print(type(c))#列表类型list
+# d={}
+# print(d)
+# print(type(d))#字典类型dict
+# print(1==False)
+# name='wangchen'
+# age=18
+# print("我的名字是%s,年龄是%d"%(name,age))
+# print('name:{},age:{}'.format(name,age))
+# name=input("请输入名字：")
+# age=input("请输入年龄")
+# print('name:{},age:{}'.format(name,age))
+a = input()
+num10 = int(a,16)
+print(a)
